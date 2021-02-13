542c31125ff194d47298701978927ab1cc642c31
Norihiro <kanjam8session@gmail.com>
2021-02-13 22:26:53 +0900

    Android初めてのコミット

---
+++ app/src/main/java/jp/techacademy/norihiro/helloandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.norihiro.helloandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
